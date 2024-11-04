ce570f51e33cd943d9264f6a34a85d48f6003a17
liulued <2267457379@qq.com>
2024-11-04 15:41:27 +0800

    第四次

---
+++ Project_SpringBoot/src/main/java/com/lixin/firstSpring/exception/test.java
@@ -0,0 +1,8 @@
+package com.lixin.firstSpring.exception;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("ceshi");
+    }
+}
